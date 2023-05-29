08444784d45b8f4c1557e90d6dbd8ba0c2010bfe
christoph.raddatz <hello@chrstphrddtz.com>
2023-05-29 09:48:46 +0200

    commented out Links in TableNavbar

--- src/app/dashboard/TableNavbar.jsx
+++ src/app/dashboard/TableNavbar.jsx
@@ -1,12 +1,12 @@
 export default function TableNavbar() {
   return (
     <div className="w-full mx-auto my-2 flex items-center justify-around flex-row bg-primary text-var(--secondaryColor) h-12 p-5 bg-white border-b">
-      <button className="text-2xl font-medium tracking-0.2rem hover:font-bold ">
+      {/* <button className="text-2xl font-medium tracking-0.2rem hover:font-bold ">
         Data Table
       </button>
       <button className="text-2xl font-medium tracking-0.2rem hover:font-bold">
         Chart View
-      </button>
+      </button> */}
     </div>
   );
 }
